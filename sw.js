757a002417800cc54e2dbfc0c4458aa36f3a8eb1
jotave <joao.v.souza193@aluno.senai.br>
2026-05-05 10:29:10 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './painel.html', 
+  './manifest.json',
+  './assets/logo.png',
+  './assets/logopwa.png',
+  './assets/logopwa512.png',
+  './assets/banner.png',
+  './assets/whatsapp.png',
+];
+
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Assando a Carne no Cache!');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Faz as requisições olharem pro cache primeiro
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })  
+  );
+});
+
+//remove caches antigos quando atualizar
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keys) => {          // ✅ caches
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)) // ✅
+      );
+    })
+  );
+});
